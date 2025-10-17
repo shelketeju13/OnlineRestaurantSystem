277bd6de3204b97dc495384d0d31950d76782a55
shelketeju13 <shelketeju13@gmail.com>
2025-10-17 15:44:49 +0530

    done

---
+++ src/main/java/com/restaurant/util/DBUtil.java
@@ -0,0 +1,21 @@
+package com.restaurant.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	 private static final String URL = "jdbc:mysql://localhost:3306/restaurant_db";
+	    private static final String USER = "root";  
+	    private static final String PASSWORD = "teju132005";  
+
+	    public static Connection getConnection() throws SQLException {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	        } catch (ClassNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	        return DriverManager.getConnection(URL, USER, PASSWORD);
+	    }
+
+}
